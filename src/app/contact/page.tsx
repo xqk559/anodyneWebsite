c2471d468c516ee569d2142260d9185b709c789e
unknown <xqk559@gmail.com>
2025-07-28 15:06:37 -0400

    Added contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
-import Link from 'next/link';
 
-export default function Home() {
+export default function ContactPage() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <header className="row-start-1 flex gap-[24px] flex-wrap w-full">
@@ -13,15 +12,17 @@ export default function Home() {
           height={38}
           priority
         />
-        <Link 
-          href="/contact"
+        <a
           className="flex gap-2 hover:underline hover:underline-offset-4 ml-auto self-center"
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Contact Us
-        </Link>
+        </a>
       </header>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        
+        <header>Contact</header>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         ©2025 by AnodyneGames. All rights reserved.
