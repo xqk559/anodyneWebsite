fb886fd6818e77faa45ab6396e375d7f1460fe0c
unknown <xqk559@gmail.com>
2025-07-28 13:45:07 -0400

    Added logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/anodyneLogoWhite.png"
+          alt="Anodyne logo"
           width={180}
           height={38}
           priority
