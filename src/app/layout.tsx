d3d570290a10c77dbe6913b0ba755a0da45b4ab9
unknown <xqk559@gmail.com>
2025-07-28 14:53:57 -0400

    Added custom font called insaniburger for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import Insaniburger from 'next/font/local';
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const insaniburger = Insaniburger({
+  src: '../fonts/Insaniburger.ttf',
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={insaniburger.className}
       >
         {children}
       </body>
