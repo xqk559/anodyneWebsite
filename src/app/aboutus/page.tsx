62ef4bc12b2c4c4ecb4f2fa4fddbd53f6b195693
unknown <xqk559@gmail.com>
2025-07-28 16:31:05 -0400

    Made Header its own component to be more reactive and changed active link color to red

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,44 +1,9 @@
-import Image from "next/image";
-import Link from 'next/link';
+import Header from '../header';
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <header className="row-start-1 flex gap-[24px] flex-wrap w-full">
-        <Link 
-          href="/"
-          className="flex gap-2 hover:underline hover:underline-offset-4 self-center"
-        >
-          <Image
-            className="dark:invert"
-            src="/anodyneLogoWhite.png"
-            alt="Anodyne logo"
-            width={180}
-            height={38}
-            priority
-          />
-        </Link>
-        <div className="flex gap-[24px] flex-wrap ml-auto">
-          <Link 
-          href="/aboutus"
-          className="flex gap-2 hover:underline hover:underline-offset-4 ml-auto self-center"
-          >
-            About Us
-          </Link>
-          <Link 
-            href="/games"
-            className="flex gap-2 hover:underline hover:underline-offset-4 ml-auto self-center"
-          >
-            Games
-          </Link>
-          <Link 
-            href="/contact"
-            className="flex gap-2 hover:underline hover:underline-offset-4 ml-auto self-center"
-          >
-            Contact Us
-          </Link>
-        </div>
-      </header>
+      <Header />
       <main className="flex flex-col row-start-2">
         <header className="text-5xl">About Us</header>
       </main>
---
+++ src/app/header.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Image from "next/image";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function Header() {
+  const pathname = usePathname();
+  return (
+    <header className="row-start-1 flex gap-[24px] flex-wrap w-full">
+      <Link 
+        href="/"
+        className="flex gap-2 hover:underline hover:underline-offset-4 self-center"
+      >
+        <Image
+          className="dark:invert"
+          src="/anodyneLogoWhite.png"
+          alt="Anodyne logo"
+          width={180}
+          height={38}
+          priority
+        />
+      </Link>
+      <div className="flex gap-[24px] flex-wrap ml-auto">
+        <Link 
+        href="/aboutus"
+        className={`flex gap-2 hover:underline hover:underline-offset-4 ml-auto self-center ${pathname == "/aboutus" ? "text-red-600" : "text-white"}`}
+        >
+          About Us
+        </Link>
+        <Link 
+          href="/games"
+          className={`flex gap-2 hover:underline hover:underline-offset-4 ml-auto self-center ${pathname == "/games" ? "text-red-600" : "text-white"}`}
+        >
+          Games
+        </Link>
+        <Link 
+          href="/contact"
+          className={`flex gap-2 hover:underline hover:underline-offset-4 ml-auto self-center ${pathname == "/contact" ? "text-red-600" : "text-white"}`}
+        >
+          Contact Us
+        </Link>
+      </div>
+    </header>
+  )
+}
